cb86b00a8d732b504d8894c7f307982b542d1eda
Gennadiy <renardes@gmail.com>
2023-08-20 23:43:25 +0500

    управляющий код верхнего уровня

--- src/main.py
+++ src/main.py
@@ -1,3 +1,39 @@
 """
 Точка входа — основной управляющий код.
 """
+
+# импорты модулей проекта
+import data
+import help
+import game
+import player
+import utils
+
+
+def start():
+    """Инициализация приложения."""
+    utils.read_players(data.players_path, data.players_db)
+    utils.read_saves(data.saves_path, data.saves_db)
+    
+    if data.players_db:
+        ...
+        # help.show_commands()
+    else:
+        ...
+        # help.show_full()
+
+
+def main_menu():
+    """Суперцикл главного меню."""
+
+
+def end():
+    """Завершение работы приложения"""
+    
+
+
+if __name__ == '__main__':
+    start()
+    main_menu()
+    end()
+
