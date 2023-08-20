838b9e022aa624d8a61ba34d19dcad816cfd78bb
Gennadiy <renardes@gmail.com>
2023-08-20 23:42:33 +0500

    функции чтения/записи файлов данных

--- src/data.py
+++ src/data.py
@@ -1,3 +1,19 @@
 """
 Глобальные переменные и условные константы.
 """
+
+# импорты модулей стандартной библиотеки
+from pathlib import Path
+from sys import argv, path
+
+
+ROOT_DIR = Path(path[0]).parent
+
+test_path = 'test/' if 'test' in argv else ''
+players_path = ROOT_DIR / f'data/{test_path}players.ini'
+saves_path = ROOT_DIR / f'data/{test_path}saves.txt'
+
+
+players_db: dict[str, dict[str, int]] = {}
+saves_db: dict[tuple[str, str], tuple[int, list[int]]] = {}
+
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,60 @@
 """
 Вспомогательные функции.
 """
+
+# импорты модулей стандартной библиотеки
+from configparser import ConfigParser
+from pathlib import Path
+from pprint import pprint
+
+
+def read_players(filein_path: Path, buffer: dict) -> None:
+    """"""
+    players_data = ConfigParser()
+    players_data.read(filein_path)
+    buffer |= {
+        player: {
+            k: int(v) 
+            for k, v in players_data[player].items()
+        }
+        for player in players_data.sections()
+    }
+
+
+def read_saves(filein_path: Path, buffer: dict) -> None:
+    """"""
+    try:
+        saves_data = filein_path.read_text(encoding='utf-8').split('\n')
+    except FileNotFoundError:
+        pass
+    else:
+        for line in saves_data:
+            try:
+                players, dim, turns = line.split('!')
+            except ValueError:
+                return
+            players = tuple(players.split(','))
+            dim = int(dim)
+            turns = [int(t) for t in turns.split(',')]
+            buffer |= {players: (dim, turns)}
+
+
+def write_players(buffer: dict, fileout_path: Path) -> None:
+    """"""
+    players_data = ConfigParser()
+    players_data.read_dict(buffer)
+    with open(fileout_path, 'w', encoding='utf-8') as fileout:
+        players_data.write(fileout)
+
+
+def write_saves(buffer: dict, fileout_path: Path) -> None:
+    """"""
+    players_data = []
+    for players, turns in buffer.items():
+        dim, turns = turns
+        players = ','.join(players)
+        turns = ','.join(str(t) for t in turns)
+        players_data.append('!'.join((players, str(dim), turns)))
+    with open(fileout_path, 'w', encoding='utf-8') as fileout:
+        fileout.write('\n'.join(players_data))
+
